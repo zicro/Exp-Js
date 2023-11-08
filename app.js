a838d3198ed30b58876a8a45eb03ff791f9bc470
xOe <larhnimikit@gmail.com>
2023-11-08 12:55:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+
+// HTTTP METHODS
+// GET - Retreive Data
+app.get('/', (request, response) => {
+    response.send('welCome to the Main app ... 0x0 !!')
+})
+app.get('/hello', (req, res) => {
+    res.send('hello world !!')
+})
+// POST 
+// PUT
+// DELETE
+
+
+app.listen(3001, () => {
+    console.log('started on port 3001')
+})
